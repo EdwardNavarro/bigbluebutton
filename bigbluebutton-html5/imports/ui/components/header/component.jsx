e0ccd73542aaaf50cb8b50008f103e6557806552
Edward Navarro <contacto@edwardnavarro.com>
2020-04-20 21:32:07 -0500

    Add new UI theme for Anáhuac University.

---
+++ bigbluebutton-html5/imports/ui/components/header/component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { styles } from "./styles";
+
+const Header = ({ text }) => (
+  <div className={styles.head}>
+    <div className={styles.logo}>
+      <img
+        src="https://umbrella-cdn.us-east-1.linodeobjects.com/logo_anahuac_bbb_top_header.png"
+        border="0"
+      />
+    </div>
+    <div className={styles.headTitle}>{text}</div>
+    <div className={styles.demoTag}>
+      <span>Demostración</span>
+    </div>
+    <div className={styles.demoTagMobile}>
+      <span>Demo</span>
+    </div>
+  </div>
+);
+
+Header.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Header;
